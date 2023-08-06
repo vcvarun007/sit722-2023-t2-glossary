83f4f5a290d6921da8cf8d15dee82c97f0fc2006
Varun Chaudhary <vcvarun007@gmail.com>
2023-08-06 12:28:06 +1000

    V2
    20 terms in glossary list

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ const tableData = [
     description:
       "Development + Operations. The mix of methods and tools aimed at enabling organizations to deliver applications and services more quickly than traditional development processes.",
     references:
-      "[1]“What Is DevOps and How Does It Work? | Synopsys,” www.synopsys.com. https://www.synopsys.com/glossary/what-is-devops.html#:~:text=Definition",
+      "“What Is DevOps and How Does It Work? | Synopsys,” www.synopsys.com. https://www.synopsys.com/glossary/what-is-devops.html#:~:text=Definition",
   },
   {
     id: 2,
@@ -25,7 +25,7 @@ const tableData = [
     description:
       "The process of automatically combining code changes from multiple contributors into one software project.",
     references:
-      "[1]Atlassian, “What is Continuous Integration,” Atlassian. https://www.atlassian.com/continuous-delivery/continuous-integration#:~:text=Continuous%20integration%20(CI)%20is%20the",
+      "Atlassian, “What is Continuous Integration,” Atlassian. https://www.atlassian.com/continuous-delivery/continuous-integration#:~:text=Continuous%20integration%20(CI)%20is%20the",
   },
   {
     id: 3,
@@ -33,7 +33,7 @@ const tableData = [
     description:
       "Automatically delivering finished code to environments such as testing and development.",
     references:
-      "[1]“What Is CI/CD and How Does It Work? | Synopsys,” www.synopsys.com. https://www.synopsys.com/glossary/what-is-cicd.html#:~:text=Continuous%20delivery%20(CD)%20is%20the (accessed Jul. 22, 2023). ‌",
+      "“What Is CI/CD and How Does It Work? | Synopsys,” www.synopsys.com. https://www.synopsys.com/glossary/what-is-cicd.html#:~:text=Continuous%20delivery%20(CD)%20is%20the (accessed Jul. 22, 2023). ‌",
   },
   {
     id: 4,
@@ -49,7 +49,7 @@ const tableData = [
     description:
       "Version control is the practice of tracking and managing changes to software code.",
     references:
-      "[1]Atlassian, “What is version control | Atlassian Git Tutorial,” Atlassian. https://www.atlassian.com/git/tutorials/what-is-version-control#:~:text=Version%20control%2C%20also%20known%20as ‌",
+      "Atlassian, “What is version control | Atlassian Git Tutorial,” Atlassian. https://www.atlassian.com/git/tutorials/what-is-version-control#:~:text=Version%20control%2C%20also%20known%20as ‌",
   },
   {
     id: 6,
@@ -57,7 +57,7 @@ const tableData = [
     description:
       "Automation involves using technology to produce and deliver goods or services with little human involvement. It improves efficiency, reliability, and speed for tasks that used to require human effort.",
     references:
-      "[1]Techopedia, “What Is Automation? - Definition from Techopedia,” Techopedia.com, Jun. 08, 2021. https://www.techopedia.com/definition/32099/automation ‌",
+      "Techopedia, “What Is Automation? - Definition from Techopedia,” Techopedia.com, Jun. 08, 2021. https://www.techopedia.com/definition/32099/automation ‌",
   },
   {
     id: 7,
@@ -65,7 +65,7 @@ const tableData = [
     description:
       "Infrastructure as Code (IaC) is the practice of managing and setting up infrastructure using code instead of manual methods.",
     references:
-      "[1]“What is Infrastructure as Code (IaC)?,” www.redhat.com. https://www.redhat.com/en/topics/automation/what-is-infrastructure-as-code-iac#:~:text=Infrastructure%20as%20Code%20(IaC)%20is ‌",
+      "“What is Infrastructure as Code (IaC)?,” www.redhat.com. https://www.redhat.com/en/topics/automation/what-is-infrastructure-as-code-iac#:~:text=Infrastructure%20as%20Code%20(IaC)%20is ‌",
   },
   {
     id: 8,
@@ -73,7 +73,7 @@ const tableData = [
     description:
       "Continuous Monitoring is the process of regularly gathering, analyzing, and utilizing information to actively improve performance, maximize positive results, and reduce the risk of negative impacts.",
     references:
-      "[1]“Monitoring | BetterEvaluation,” www.betterevaluation.org. https://www.betterevaluation.org/methods-approaches/themes/monitoring#:~:text=Monitoring%20is%20a%20process%20to ‌",
+      "“Monitoring | BetterEvaluation,” www.betterevaluation.org. https://www.betterevaluation.org/methods-approaches/themes/monitoring#:~:text=Monitoring%20is%20a%20process%20to ‌",
   },
   {
     id: 9,
@@ -81,7 +81,7 @@ const tableData = [
     description:
       "Incident Management is the process used by IT Operations and DevOps teams to respond to and address unplanned events that can impact service quality or service operations.",
     references:
-      "[1]“What Is Incident Management?,” www.ibm.com. https://www.ibm.com/cloud/blog/what-is-incident-management#:~:text=Incident%20management%20is%20a%20process ‌",
+      "“What Is Incident Management?,” www.ibm.com. https://www.ibm.com/cloud/blog/what-is-incident-management#:~:text=Incident%20management%20is%20a%20process ‌",
   },
   {
     id: 10,
@@ -89,7 +89,87 @@ const tableData = [
     description:
       "Agile is a continuous improvement approach that involves a flexible, collaborative, and value-driven methodology. Easy Agile develops Jira apps that help teams work better with agile, offering simple and flexible functionality.",
     references:
-      "[1]“Agile 101: A Beginner’s Guide to Agile Methodology,” Easy Agile. https://www.easyagile.com/blog/agile-101/#:~:text=In%20basic%20terms%2C%20agile%20is (accessed Jul. 22, 2023). ‌",
+      "“Agile 101: A Beginner’s Guide to Agile Methodology,” Easy Agile. https://www.easyagile.com/blog/agile-101/#:~:text=In%20basic%20terms%2C%20agile%20is (accessed Jul. 22, 2023). ‌",
+  },
+  {
+    id: 11,
+    term: "Artifact",
+    description:
+      "A ready-to-use output produced from code, like a software package ready for installation.",
+    references:
+      "“Managing DevOps Artifacts,” docs.oracle.com. https://docs.oracle.com/en-us/iaas/Content/devops/using/artifacts.htm#:~:text=DevOps%20artifacts%20are%20referenced%20in (accessed Aug. 06, 2023).",
+  },
+  {
+    id: 12,
+    term: "Pipeline",
+    description:
+      "A sequence of automated steps that build and deliver code, like a streamlined process for software development.",
+    references:
+      "Atlassian, “DevOps Pipeline,” Atlassian. https://www.atlassian.com/devops/devops-tools/devops-pipeline#:~:text=What%20is%20the%20DevOps%20pipeline (accessed Aug. 06, 2023).",
+  },
+  {
+    id: 13,
+    term: "Scalability",
+    description:
+      "The ability of a system to handle more work as needed, like a flexible team adjusting to larger projects.",
+    references:
+      "“Efficient methods for scaling DevOps & approaches to overcome challenges | ReviewNPrep,” Jul. 24, 2022. https://reviewnprep.com/blog/efficient-methods-for-scaling-devops-approaches-to-overcome-challenges/#:~:text=Scalability%20refers%20to%20the%20ability (accessed Aug. 06, 2023).",
+  },
+  {
+    id: 14,
+    term: "Release Management",
+    description:
+      "Organized planning and control of software updates, like a scheduled and controlled deployment process.",
+    references:
+      "“DevOps Release Management Concepts & Best Practices,” Splunk-Blogs. https://www.splunk.com/en_us/blog/learn/devops-release-management.html#:~:text=In%20a%20DevOps%20environment%2C%20release (accessed Aug. 06, 2023).",
+  },
+  {
+    id: 15,
+    term: "Versioning",
+    description:
+      "Assigning unique names to different versions of code, like labeling different editions of a book.",
+    references:
+      "“What is versioning and how does it work?,” Software Quality. https://www.techtarget.com/searchsoftwarequality/definition/versioning#:~:text=In%20software%20development%2C%20versioning%20allows (accessed Aug. 06, 2023).",
+  },
+  {
+    id: 16,
+    term: "Change Management",
+    description:
+      "The careful handling and implementation of modifications, like following an approved plan for changes.",
+    references:
+      "“Best practices for change management in the age of DevOps,” Atlassian Engineering, May 12, 2020. https://www.atlassian.com/engineering/best-practices-for-change-management-in-the-age-of-devops",
+  },
+  {
+    id: 17,
+    term: "Continuous Improvement",
+    description:
+      "The ongoing process of getting better, like regular practice to enhance a skill.",
+    references:
+      "“Continuous improvement | DevOps | TMap,” www.tmap.net. https://www.tmap.net/building-blocks/Continuous-improvement-DevOps",
+  },
+  {
+    id: 18,
+    term: "Microservices",
+    description:
+      "Building applications from small, independent parts, like dividing a complex task into smaller, manageable steps.",
+    references:
+      "“What are Microservices? | AWS,” Amazon Web Services, Inc. https://aws.amazon.com/microservices/#:~:text=Microservices%20are%20an%20architectural%20and",
+  },
+  {
+    id: 19,
+    term: "Service Discovery",
+    description:
+      "Automatically finding and connecting to services, like a smartphone connecting to available Bluetooth devices.",
+    references:
+      "“What is Service Discovery? Definition and Related FAQs,” Avi Networks. https://avinetworks.com/glossary/service-discovery/#:~:text=Service%20discovery%20is%20the%20process (accessed Aug. 06, 2023).",
+  },
+  {
+    id: 20,
+    term: "ChatOps",
+    description:
+      "Using chat tools to collaborate and control tasks, like a team using a chat group for efficient teamwork.",
+    references:
+      "“What is ChatOps? | Definition from TechTarget,” IT Operations. https://www.techtarget.com/searchitoperations/definition/ChatOps#:~:text=ChatOps%2C%20sometimes%20known%20as%20conversation (accessed Aug. 06, 2023).",
   },
 ];
 
